8b60bb2992bf8aaf81fd33343bbab83bfb490ccc
khangbaohua <khang.hua.cool@gmail.com>
2023-09-14 19:31:02 -0500

    print, input

---
+++ basic.py
@@ -0,0 +1,7 @@
+# print("hello")
+# name = input ("enter your name: ")
+# print(name)
+# age = int(input("enter your age: " ))
+# print(age)
+grade = float(input("enter your grade:" ))
+print(grade)

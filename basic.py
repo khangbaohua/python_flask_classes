dc8f6c36eb3eb1ab9ee30dcbb332732ba66fb9e3
khangbaohua <khang.hua.cool@gmail.com>
2023-09-19 19:28:20 -0500

    list, list slicing

--- basic.py
+++ basic.py
@@ -3,5 +3,43 @@
 # print(name)
 # age = int(input("enter your age: " ))
 # print(age)
-grade = float(input("enter your grade:" ))
-print(grade)
+# grade = float(input("enter your grade:" ))
+# print(grade)
+# Data types 
+# int = integer numbers 
+# float = number that has decimal point
+# bool = true or false
+# string = paragraphs, names, page, line
+# char = single character
+# lists 
+# print(python)
+# print(python[0])
+# print(python[4]
+# python[0] = "Hua"
+# print(python[0])
+# python[4] = 23.5
+# print(python[4])
+# list slicing
+# print(python[2:5])
+# print(python[1:3])
+# print(python[4:6])
+# print(python[0:3])
+# print(python[1:6:2])
+# print(python[0:5:2])
+# print(python[0:4:3])
+# print(python[3:6:2])
+# print(python[0:6:5])
+# print(python[-4])
+# print(python[-4:-1])
+# print(python[-5:-2])
+# print(python[-6:-2:3])
+# print(python[-3::2])
+python = ["khang",45,True,'c',92,23.5]
+# print(python[-2:])
+# print(python[-1:-6:-2])
+# print(python[-1:-6:-2])
+# print(python[5:0:-2])
+print(python[])
+# 2) 23.5 45
+# 3) True "khang"
+# 4) 92, 23.5

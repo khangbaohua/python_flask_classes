1b9fe6e4eff10b44397a5677232bcfcbd79919c9
khangbaohua <khang.hua.cool@gmail.com>
2023-09-21 19:18:37 -0500

    strings, if/else

--- basic.py
+++ basic.py
@@ -34,12 +34,160 @@
 # print(python[-5:-2])
 # print(python[-6:-2:3])
 # print(python[-3::2])
-python = ["khang",45,True,'c',92,23.5]
+# python = ["khang",45,True,'c',92,23.5]
 # print(python[-2:])
 # print(python[-1:-6:-2])
 # print(python[-1:-6:-2])
 # print(python[5:0:-2])
-print(python[])
+# print(python[])
 # 2) 23.5 45
 # 3) True "khang"
 # 4) 92, 23.5
+# string
+# mystring = "string notes for python"
+# every character has index
+# print(mystring[0:12])
+# naming conventions of variables
+# lowercase and uppercase letter are allowed in variable name anywhere
+# you can only use numbers anywhere else except for the start of the name
+# you can use underscore symbol
+# you can not use spaces while naming variable
+# conditional statements
+
+# age = 180
+# if age > 18:
+#     print("you can vote")
+# elif age==18:
+#     print("go and apply for voter id card")
+# else:
+#     print("you cannont vote")
+
+# if 3 == 4:
+#     print("Equal")
+# elif (3 > 4):
+#     print("greater")
+# else:
+#     print("less")
+
+# if 13 > 4:
+#     print("greater")
+# elif 13 < 4:
+#     print("less")
+# else:
+#     print("equal")
+
+# x = 4 
+# if x == 0:
+#     print("equal")
+# elif x >= 4:
+#     print("greater or equal")
+# elif x > 4:
+#     print("greater")
+# else:
+#     print("default") x = 3 
+
+# if x == 0:
+#     print("equal")
+# elif x >= 4:
+#     print("greater or equal")
+# elif x > 4:
+#     print("greater")
+# else:
+#     print("default")
+
+# x = 3 
+# if x == 0:
+#     print("equal")
+# elif x >= 4:
+#     print("greater or equal")
+# elif x > 4:
+#     print("greater")
+# else:
+#     print("default")
+
+# x = 3 
+# if x == 0:
+#     print("equal")
+# elif x >= 4:
+#     print("greater or equal")
+# elif x > 4:
+#     print("greater")
+# else:
+#     print("default")
+
+# x = 0
+# if x == 0:
+#     print("equal")
+# elif x >= 4:
+#     print("greater or equal")
+# elif x > 4:
+#     print("greater")
+# else:
+#     print("default")
+
+# x = 2
+# if x == 0:
+#     print("equal")
+# elif x <= 4:
+#     print("less or equal")
+# elif x < 4:
+#     print("less")
+# else:
+#     print("default")
+
+# if not(5 < 4):
+#     print("one")
+# elif 4 > 3:
+#     print("two")
+# else:
+#     print("none")
+# if not(4 > 6) and not(8 > 5):
+#     print("one")
+# elif 4 > 5 or not(5 == 5):
+#     print("two")
+# else:
+#     print("none")
+# if 4 > 6 and not(8 > 5):
+#     print("one")
+# elif not(4 > 5) or not(5 == 5):
+#     print("two")
+# else:
+#     print("none")
+
+# x = 3
+# y = 12
+# if x <= y and x > 0:
+#     print("three")
+# if x > y or y < 11:
+#     print("four")
+# if not(x < 4) and y < 24:
+#     print("five")
+# elif y != 4:
+#     print("six")
+# elif y >= x:
+#     print("seven")
+# else:
+#     print("default")
+
+# x = 12
+# y = 3
+# z = 11
+# if (x == 2 and x < 3):
+#     print(x)
+# if x != 5:
+#     print("whatever")
+# if x != 5 and y >= 5 and z <= 13:
+#     print("its gonna happen")
+# if z != 0 or x == 2:
+#     print("you say")
+# if (not(y < 10)):
+#     print(y)
+# elif(x < 10 or x < 5):
+#     print("okkkkkkk")
+# elif(y < 10 or y <= 0):
+#     print("pikachu")
+# elif(z == 0 or y ==5):
+#     print("Pikaboo")
+# else:
+#     print("Default value")
+

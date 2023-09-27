58f9e151c6c97da95dee408234b993cda3617680
khangbaohua <khang.hua.cool@gmail.com>
2023-09-26 19:16:35 -0500

    function

--- basic.py
+++ basic.py
@@ -191,3 +191,47 @@
 # else:
 #     print("Default value")
 
+# for loop
+# for i in range(1,11):
+#     print(i)
+
+# for i in range(30,61):
+    # print(i)
+
+# for i in range(1,31):
+#     if i%2==0:
+#         print(i)
+
+# for i in range(1,31):
+#     if i%2==1:
+#         print(i)
+# % called modulo find remainder
+# for i in range(1,11):
+#     print(3*i)
+
+# for i in range(1,11):
+#     print(11*i)
+# for i in range(1,11):
+#     if i%3==0:
+#         print(4*i)
+
+# for i in range(1,11):
+#     if i%2==1:
+#         print(13*i)
+
+# num = int(input("enter a number"))
+# for i in range(2,13):
+#     if num%i==0:
+#         print("composite")
+#         break
+#     if num%i==1:
+#         print("prime")
+#         break
+
+num = int(input("put multiplication problem: "))
+for i in range(1,2):
+    if num%i==1:
+        print("positive")
+    if num%i==-1:
+        print("negative")
+

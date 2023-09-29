0512de233198d025363605db3163981464461c5a
khangbaohua <khang.hua.cool@gmail.com>
2023-09-28 19:13:52 -0500

    list functions

--- basic.py
+++ basic.py
@@ -228,10 +228,101 @@
 #         print("prime")
 #         break
 
-num = int(input("put multiplication problem: "))
-for i in range(1,2):
-    if num%i==1:
-        print("positive")
-    if num%i==-1:
-        print("negative")
+# num = int(input("put multiplication problem: "))
+# for i in range(1,2):
+#     if num%i==1:
+#         print("positive")
+#     if num%i==-1:
+#         print("negati 
 
+# list functions
+# mylist[1] = 84
+# print(mylist)
+
+# mylist[3] = "True"
+# print(mylist)
+
+# mylist.append(93.5)
+# print(mylist)
+
+# mylist.append("Ethan")
+# print(mylist)
+
+# mylist.insert(1,2)
+# print(mylist)
+
+# mylist.insert(4, 98.9)
+# print(mylist)
+
+# mylist.remove(123)
+# print(mylist)
+
+# mylist.remove("list")
+# print(mylist)
+
+# mylist.pop(2)
+# print(mylist)
+
+# mylist.pop(3)
+# print(mylist)
+# del mylist[2]
+# print(mylist)
+
+# mylist.sort()
+# print(mylist)
+
+# mylist.sort(reverse=True)
+# print(mylist)
+
+# mylist.reverse()
+# print(mylist)
+
+
+# mylist = [321, 123, 5, 45, 5, 5, 45, 45, 45, 321, 123]
+# c = mylist.count(5)
+# print(c)
+
+# i = mylist.index(45)
+# print(i)
+
+# mylist.clear()
+# print(mylist)
+
+# newlist = mylist.copy()
+# print(newlist)
+
+# coollist = ["khang", "cool", 238, 98, "python"]
+# mylist.extend(coollist)
+# print(mylist)
+
+# mylist.append() adds thing to list adds to end of list
+# mylist.insert(#,#) add thing to list in between of a number
+# mylist.remove() removes thing from list, dont need to put index just put the word/number
+# mylist.pop(index) removes using index instead of word/number
+# mylist.sort() sorts number or words in least to greatest only works with only numbers or onlt words
+# mylist.reverse() sorts number or words in greatest to least, same rules as sort
+#(variable) = mylist.count() counts how many of the same number/words there are
+# (variable) = mylist.index() tells what index a number/word is
+# mylist.clear() clears the whole list
+# newlist = mylist.copy() makes a new list and copies everything from the list that is already present
+# mylist.extend(newlistname) combines a new list and another list together
+newlist = [23, 42, 23.5, 43.5, 23, 23, 5]
+# newlist.insert(3, "True")
+# print(newlist)
+# newlist.remove(23.5)
+# print(newlist)
+# newlist.pop(2)
+# print(newlist)
+# mylist[5]=50.4
+# print(newlist)
+# newlist.sort(reverse=True)
+# print(newlist)
+# c = newlist.count(23)
+# print(c)
+# coollist = newlist.copy()
+# print(coollist)
+# coollist = [2, 2, 2]
+# newlist.extend(coollist)
+# print(newlist)
+newlist.reverse()
+print(newlist)

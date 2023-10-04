fb6306f8e88cc3c711920c4b2e3c9e11862eaf8f
khangbaohua <khang.hua.cool@gmail.com>
2023-10-03 20:57:56 -0500

    string slicing and function

---
+++ strings.py
@@ -0,0 +1,36 @@
+# 2) 23.5 45
+# 3) True "khang"
+# 4) 92, 23.5
+# string
+# every character has index
+# print(mystring[0:12])
+# print(mystring[2:12])
+# print(mystring[-21:-11])
+# print(mystring[2:-11])
+# print(mystring[-21:12])
+# print(mystring[-4:-20:-3])
+# print(mystring[19:3:-3])
+# print(mystring[19:-20:-3])
+# print(mystring[-4:3:-3])
+# print(mystring[0:19:2])
+# print(mystring[-23:-4:2])
+# print(mystring[0:-4:2])
+# print(mystring[-23:19:2])
+# print(mystring[10:23:2])
+# print(mystring[-13::2])
+# print(mystring[:])
+# if you put nothing as end index, it would by default include everything till the end
+# if you put nothing as start index, it would by default be 0 
+# 
+
+# mystring = "stringnotesforpython1.2"
+# string functions
+# print(mystring.capitalize())
+# print(mystring.count(" "))
+# # print(mystring.index("s"))
+# print(mystring.endswith("string notes for python"))
+# print(mystring.isalnum())
+# mystring = "%i am@khang"
+# for i in mystring:
+#     if i.isalnum():
+#         print(i,end=":")

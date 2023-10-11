feaba32d79ad2cdd082393e1c30a7956a6b4262f
khangbaohua <khang.hua.cool@gmail.com>
2023-10-10 21:06:13 -0500

    test

---
+++ test.py
@@ -0,0 +1,40 @@
+# task 1
+# print("khang 12")
+# my_age = int(input("12"))
+# print(my_age+10)
+# task 2
+# favorite_animal = "kangaroo"
+# x = favorite_animal.index("kangaroo")
+# print(x)
+# print(favorite_animal[0])
+# print(favorite_animal[7])
+# task 3
+# fruits = ["apple", "banana", "cherry"]
+# for i in fruits:
+#     print(i)
+
+# # task 4
+# num1 = int(input("enter a number: "))
+# num2 = int(input("enter a number: "))
+# c = num1+num2
+# print(c)
+# x = num1-num2
+# print(x)
+# p = num1/num2
+# print(p)
+# u = num1*num2
+# print(u)
+# task 5
+# x=5
+# print(x)
+# x+=3
+# print(x)
+# x-=2
+# print(x)
+# # task 6
+num3 = int(input("enter a number"))
+num4
+print(67+23)
+print(671-258)
+print(67*24)
+print(67/26)

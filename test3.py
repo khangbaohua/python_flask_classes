5cf5786e695b748eb161ee4d1adb26d5450ac057
khangbaohua <khang.hua.cool@gmail.com>
2023-12-07 19:25:21 -0600

    test

---
+++ test3.py
@@ -0,0 +1,49 @@
+# conditional statement
+# age = input("enter age:")
+# for i in age:
+#     if age<13:
+#         print("kid")
+#     if age in range(13,19):
+#         print("teen")
+#     if age==20 or age>20:
+#         print("adult")
+
+# list and loop
+# animal = ["kangaroo","mountain lion","giraffee"]
+# for i in animal:
+#     print(i)
+# print(len(animal[2]))
+
+# function
+
+# def cast_spell(a):
+#     print(a*a)
+# cast_spell(8)
+
+# sets
+# set1 = {"math","literature","algebra"}
+# set2 = {"math","english","history"}
+# set1.intersection_update(set2)
+# print(set1)
+
+# operators and conditions
+# number = int(input("enter number:"))
+# for i in number:
+#     if i==-1:
+#         print("negative")
+#     if i==1:
+#         print("posotive")
+#     if i==0:
+#         print("Zero")
+
+
+# strings
+# string = ("a","e","i","o","u")
+# print(len(string))
+# print(string)
+
+# bonus
+# def num(a):
+#     for i in range(1,11):
+#         print(a*i)
+# num(2)

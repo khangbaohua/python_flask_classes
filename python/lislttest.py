9154e493a6c2334a926338aec6c627cc09e0adf9
khangbaohua <khang.hua.cool@gmail.com>
2024-05-14 20:29:01 -0500

    list test

---
+++ python/lislttest.py
@@ -0,0 +1,25 @@
+# Find the Sum of All Elements in a List
+# Write a program that takes a list of numbers and outputs the sum of all the elements in the list.
+list = [4,6,3,2,78,6]
+# sum = sum(list)
+# print(sum)
+
+# Find the Maximum and Minimum Element in a List
+# Create a function that accepts a list of numbers and returns the maximum and minimum elements in the list
+# max = max(list)
+# min = min(list)
+# print(max)
+# print(min)
+
+# Check if an Element Exists in a List
+# Write a program where the user can input an element, and the program checks if this element exists in a pre-defined list.
+# input = int(input("Enter number: "))
+# if input in list:
+#     print(True)
+# else:
+#     print(False)
+#
+# Count Occurrences of an Element in a List
+# Create a function that counts how many times a particular element appears in a list.
+count = list.count(6)
+print(count)
--- python/test5.py
+++ python/test5.py
@@ -108,12 +108,115 @@ Mystr = "Hello 42587896591978439785445"
 # print(sum)
 
 # You are given a string which contains numbers and alphabets. I want the average of all the numbers which are even
-count = 0
-sum = 0
-for i in Mystr:
-    if i.isdigit() and int(i)%2==0:
-        sum+=int(i)
-        count+=1
-average = sum/count
-print(average)
+# count = 0
+# sum = 0
+# for i in Mystr:
+#     if i.isdigit() and int(i)%2==0:
+#         sum+=int(i)
+#         count+=1
+# average = sum/count
+# print(average)
     
+# Print Even Numbers:Write a program that uses a for loop to print all even numbers from 2 to 20.
+# for i in range(2,21):
+#     if i%2==0:
+#         print(i)
+
+# Print Odd Numbers:
+# Use a for loop to print all odd numbers from 1 to 19.
+# for i in range(1,20):
+#     if i%2!=0:
+#         print(i)
+
+# Print Numbers in Reverse:
+# Write a program that uses a for loop to print numbers from 10 to 1 in reverse order.
+# for i in reversed(range(1,11)):
+#     print(i)
+# or 
+# for i in range(11,0,-1):
+#     print(i)
+
+# Count By Fives:
+# Use a for loop to print numbers from 5 to 50, counting by fives
+
+# for i in range(5,51,5):
+#     print(i)
+
+# Write a program that uses a for loop to print numbers from -1 to -10.
+# for i in range(-1,-11,-1):
+#     print(i)
+
+# Double and Print Numbers:
+# Write a program that doubles numbers from 1 to 10 and prints each result using a for loop.
+
+# for i in range(1,11,):
+#     double = i*2
+#     print(double)
+
+# Print the First 10 Multiples of a Given Number:
+# Ask the user for a number and use a for loop to print the first 10 multiples of that number.
+
+# number = int(input("Enter a number: "))
+# print(number)
+# for i in range(1,11):
+#     print(number * i)
+
+# Countdown with a Twist:
+# Write a program that uses a for loop to print a countdown from 20 to 1, but only print the numbers that are divisible by 3.
+
+# for i in range(20,0,-1):
+#     if i%3==0:
+#         print(i)
+
+# Print the First 10 Square Numbers:
+# Use a for loop to print the squares of the numbers from 1 to 10 (i.e., 1, 4, 9, ..., 100).
+# for i in range(1,11):
+#     squared = i**2
+#     print(squared)
+
+# List Indexes:
+# Given a list of colors (e.g., ["red", "blue", "green"]), use a for loop to print each color and its corresponding index in the list.
+# list = ["red","blue","green"]
+# for i in range(len(list)):
+#     print("i:", i, "color:",list[i])
+
+# Print Each Character:
+# Write a program that uses a for loop to print each character of a given string one by one.
+# string =   "askdkjsdhkladh"
+# for i in string:
+#     print(i)
+
+# Count Specific Character:
+# Ask the user for a string and a character, then use a for loop to count how many times that character appears in the string.
+# string = input("Enter string: ")
+# char = input("enter char: ")
+# count = 0
+# for i in string:
+#     if char == i:
+#         count+=1
+# print(count)
+
+# Reverse a String:
+# Write a program that uses a for loop to reverse a string and print the reversed version.
+
+# string = "looc"
+# string2 = ""
+# for i in range(len(string)-1,-1,-1):
+#     string2 += string[i]
+# print(string2)
+
+# Print Characters at Even Indices:
+# Write a program that prints characters located at even indices in a given string using a for loop.
+# string = "super cool"
+# evenletters = ""
+# for i in range(len(string)):
+#     if i%2==0:
+#         evenletters+=string[i]
+# print(evenletters)
+# Capitalize Each Word:
+# Use a for loop to go through a string and capitalize the first letter of each word. Assume the words are separated by spaces.
+strin = "amazing banana holder"
+upper = ""
+for i in strin.split():
+    upper+=i.capitalize()
+print(upper)
